3c6a0ba7fad80131d37a7bfb4d6776db1c59b4cf
Federico Lois <federico.lois@corvalius.com>
2017-09-19 12:03:50 -0300

    Importing blocks and transactions without linking previous blocks and input data.

---
+++ src/Sample.Blockchain/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Diagnostics;
+using CommandLine;
+using CommandLine.Text;
+using NBitcoin;
+using NBitcoin.BitcoinCore;
+using System.Linq;
+using System.IO;
+using Raven.Client.Documents;
+using Raven.Client.Exceptions;
+using Raven.Client.ServerWide;
+using Raven.Client.ServerWide.Operations;
+
+namespace Sample.Blockchain
+{
+    public class SampleOptions
+    {
+        [Option("Blocks", HelpText = "The path to the block file", Required = true, DefaultValue = @"data\blocks")]
+        public string BlocksPath
+        {
+            get;
+            set;
+        }
+
+        [Option("Url", HelpText = "The server url", Required = true, DefaultValue = @"http://localhost:8080")]
+        public string ServerUrl
+        {
+            get;
+            set;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var options = Parser.Default.ParseArguments<SampleOptions>(args);
+            if ( options.Errors.Any() )
+            {
+                Console.WriteLine(GetUsage(options));
+                return;
+            }
+
+            var directory = new DirectoryInfo(options.Value.BlocksPath);
+            if ( !directory.Exists )
+            {
+                Console.WriteLine("Directory for reading blocks doesn't exist.");
+                Console.WriteLine();
+                Console.WriteLine(GetUsage(options));
+                return;
+            }
+
+            var databaseRecord = new DatabaseRecord("BlockChain");
+
+            var store = new DocumentStore
+            {
+                Urls = new[] { options.Value.ServerUrl },
+                Database = databaseRecord.DatabaseName
+            };
+            store.Initialize();
+
+
+            try
+            {
+                store.Admin.Server.Send(new CreateDatabaseOperation(databaseRecord));
+            }
+            catch (ConcurrencyException)
+            {
+                // Nothing to be done here, the database already exist.
+            }
+
+            var blockStore = new BlockStore(directory.FullName, Network.Main);
+            var bulkInsert = store.BulkInsert();
+
+            int i = 0;
+            //foreach (var blk in blockStore.EnumerateFile(Path.Combine(directory.FullName, "blk00958.dat")))            
+            foreach (var blk in blockStore.EnumerateFolder())
+			{
+                var blockModel = new Block(blk.Item);
+			    bulkInsert.Store(blockModel);
+
+                foreach (var tx in blk.Item.Transactions)
+			    {
+			        var txModel = new Transaction(blk.Item, tx);
+			        bulkInsert.Store(txModel);
+                }
+
+                i++;
+            }
+        }
+
+        public static string GetUsage(ParserResult<SampleOptions> results)
+        {
+            return HelpText.AutoBuild(results, (HelpText current) => HelpText.DefaultParsingErrorsHandler(results, current));
+        }
+    }
+}
---
+++ src/Sample.Blockchain/Utils.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Text;
+using NBitcoin;
+
+namespace Sample.Blockchain
+{
+    public static class Utils
+    {
+        public static ulong ToUnixTime(this DateTimeOffset target)
+        {
+            var date = target.ToUniversalTime();
+            var unixTimestamp = (target - date).TotalSeconds;
+
+            return (ulong)unixTimestamp;
+        }
+
+        public static LockType GetLockType(this NBitcoin.Transaction tx)
+        {
+            if (tx.LockTime.IsHeightLock)
+                return LockType.Height;
+            if (tx.LockTime.IsTimeLock)
+                return LockType.Time;
+            return LockType.None;
+        }
+
+        public static AddressType GetAddressType(this NBitcoin.TxOut tx)
+        {
+            var template = tx.ScriptPubKey.FindTemplate();
+
+            if (template != null)
+            {
+                switch (template.Type)
+                {
+                    case TxOutType.TX_PUBKEY: return AddressType.PublicKey;
+                    case TxOutType.TX_PUBKEYHASH: return AddressType.PublicKeyHash;
+                    case TxOutType.TX_MULTISIG: return AddressType.MultiplePublicKeyHashes;
+                    case TxOutType.TX_SCRIPTHASH: return AddressType.ScriptHash;
+                }
+            }
+
+            var scriptAddress = tx.ScriptPubKey.GetScriptAddress(Network.Main);
+            if (scriptAddress != null)
+            {
+                switch (scriptAddress.Type)
+                {
+                    case Base58Type.SCRIPT_ADDRESS: return AddressType.ScriptHash;
+                    case Base58Type.PUBKEY_ADDRESS: return AddressType.PublicKey;
+                }
+            }
+            
+            throw new NotSupportedException("Not supported yet");
+        }
+
+        public static AddressType GetAddressType(this NBitcoin.TxIn tx)
+        {
+            var type = tx.ScriptSig.FindTemplate().Type;
+            switch (type)
+            {
+                case TxOutType.TX_PUBKEY: return AddressType.PublicKey;
+                case TxOutType.TX_PUBKEYHASH: return AddressType.PublicKeyHash;
+                case TxOutType.TX_MULTISIG: return AddressType.MultiplePublicKeyHashes;
+                case TxOutType.TX_SCRIPTHASH: return AddressType.ScriptHash;
+            }
+
+            throw new NotSupportedException("Not supported yet");
+        }
+
+    }
+}
